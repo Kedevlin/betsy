7248a7bc01a642cba1967cde74715b0a8cf4b580
Desiree <desireepoland@gmail.com>
2016-01-21 14:28:49 -0800

    Add length, width, height, and weight to products model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121193210) do
+ActiveRecord::Schema.define(version: 20160121222752) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -69,6 +69,10 @@ ActiveRecord::Schema.define(version: 20160121193210) do
     t.boolean  "active",      default: true
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "length"
+    t.integer  "width"
+    t.integer  "height"
+    t.integer  "weight"
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"
