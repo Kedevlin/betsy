6605d7219c0735ad8d3e5e7e7bd19423e5152655
Desiree <desireepoland@gmail.com>
2016-01-21 21:45:12 -0800

    Update order model specs to pass

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -21,10 +21,6 @@ RSpec.describe Order, type: :model do
     it { is_expected.to validate_presence_of(:city) }
     it { is_expected.to validate_presence_of(:state) }
     it { is_expected.to validate_presence_of(:zip) }
-    it { is_expected.to validate_presence_of(:cc_num) }
-    it { is_expected.to validate_presence_of(:cc_exp) }
-    it { is_expected.to validate_presence_of(:cc_cvv) }
-    it { is_expected.to validate_presence_of(:cc_name) }
 
     it "must have a valid email " do
       expect(Order.create(good_params.merge(email: "user_at_foo.org"))).to_not be_valid
@@ -34,6 +30,20 @@ RSpec.describe Order, type: :model do
       expect(Order.create(good_params.merge(email: " "))).to_not be_valid
       expect(Order.create(good_params.merge(email: "kelly@kelly.com"))).to be_valid
     end
+  end
+
+  describe ".validates second step" do
+    subject do
+      o = Order.new
+      o.current_step = "billing"
+      o
+    end
+
+    it { is_expected.to validate_presence_of(:cc_num) }
+    it { is_expected.to validate_presence_of(:cc_exp) }
+    it { is_expected.to validate_presence_of(:cc_cvv) }
+    it { is_expected.to validate_presence_of(:cc_name) }
+
   end
 
   describe "#total_cost" do
