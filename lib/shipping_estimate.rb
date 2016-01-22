0c97e537139638938cb82cb07b9f9375e8376bb3
Desiree <desireepoland@gmail.com>
2016-01-22 10:54:25 -0800

    Rearranging to work with shippingservice api

--- lib/shipping_estimate.rb
+++ lib/shipping_estimate.rb
@@ -5,7 +5,8 @@ class ShippingEstimate
     # from order get destination address info
     destination = [order.country, order.state, order.city, order.postal_code]
 
-    # for each user/merchant, do HTTParty requests and add the estimate to results
+    # for each user/merchant, make packages
+    packages = []
     order_items.group_by { |oi| oi.product.user }.each do |user, oitems|
     # user is the merchant & oitems are all order items from that merchant/user
       # from user/merchant get origin address info
@@ -16,11 +17,15 @@ class ShippingEstimate
         p_item = [oi.product.weight, oi.product.height, oi.product.length, oi.product.width]
         package_items << p_item
       end
-      # send HTTParty request to api with above info
-          # result/estimated_cost = HTTParty.get.....
-      # update the result hash with data recieved from http party / api
-          ## result["UPS 3Day"/type] += estimated_cost[ups type]
+      packages << [origin, package_items]
     end
+
+    #do HTTParty requests and add the estimate to results
+    # send HTTParty request to api with above info
+        # result/estimated_cost = HTTParty.get.....
+    # update the result hash with data recieved from http party / api
+        ## result["UPS 3Day"/type] += estimated_cost[ups type]
+
     return result = {"UPS 3Day" => 0, "USPS 2Day" => 1}
   end
 end
